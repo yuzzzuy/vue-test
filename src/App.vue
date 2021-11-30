843e533d266f1cacfa7b95c18de122f634ff0009
雨中薰风 <yuzhongxunfeng@gmail.com>
2021-11-30 11:01:51 +0800

    todo list

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,106 @@
 <template>
   <div id="app">
-    <School/>
-    <hr>
-    <Student/>
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <MyHeader :addTodo="addTodo"/>
+        <MyList :todos="todos" :checkTodo="checkTodo" :deleteTodo="deleteTodo"/>
+        <MyFooter :todos="todos" :checkAllTodo="checkAllTodo" :clearAll="clearAll"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import School from "@/components/test1/School";
-import Student from "@/components/test1/Student";
+
+import MyHeader from "@/components/text2/MyHeader";
+import MyFooter from "@/components/text2/MyFooter";
+import MyList from "@/components/text2/MyList";
 
 export default {
   name: 'App',
-  components: {
-    Student,
-    School,
+  components: {MyList, MyFooter, MyHeader},
+  data() {
+    return {
+      todos: JSON.parse(localStorage.getItem("todos")) ||
+          [{id: '001', title: '抽烟', done: true},
+            {id: '002', title: '喝酒', done: false},
+            {id: '003', title: '烫头', done: true},]
+    }
+  },
+  methods: {
+    //添加一个todo
+    addTodo(todoObj) {
+      this.todos.unshift(todoObj)
+    },
+    checkTodo(id) {
+      this.todos.forEach((todo) => {
+        if (todo.id === id) todo.done = !todo.done
+      })
+    },
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    checkAllTodo(done) {
+      this.todos.forEach(e => e.done = done);
+    },
+    clearAll() {
+      this.todos = this.todos.filter(todo => todo.done === false)
+    }
+  },
+  watch: {
+    todos: {
+      deep: true,
+      handler(value) {
+        localStorage.setItem("todos", JSON.stringify(value));
+      }
+    }
   }
+
 }
 </script>
+
+<style>
+/*base*/
+body {
+  background: #fff;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
